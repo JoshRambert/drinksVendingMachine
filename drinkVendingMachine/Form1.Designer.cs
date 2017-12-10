d925f8931254b6e37438e1ca47b637225d159ab5
JoshRambert <Joshua_rambert@yahoo.com>
2017-12-09 18:36:47 -0800

    intial commit

---
+++ drinkVendingMachine/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace drinkVendingMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cokePrice = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cokeDrinks = new System.Windows.Forms.Label();
+            this.cokePictureBox = new System.Windows.Forms.PictureBox();
+            this.spritePictureBox = new System.Windows.Forms.PictureBox();
+            this.spritePrice = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.spriteDrinks = new System.Windows.Forms.Label();
+            this.mountainDewPictureBox = new System.Windows.Forms.PictureBox();
+            this.dewPrice = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dewDrinks = new System.Windows.Forms.Label();
+            this.jarritosPictueBox = new System.Windows.Forms.PictureBox();
+            this.sevenUpPictureBox = new System.Windows.Forms.PictureBox();
+            this.jarPrice = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.jarritosDrinks = new System.Windows.Forms.Label();
+            this.sevenPrice = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.sevenUpDrinks = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.salesLabel = new System.Windows.Forms.Label();
+            this.cokeNameLabel = new System.Windows.Forms.Label();
+            this.spriteNameLabel = new System.Windows.Forms.Label();
+            this.dewNameLabel = new System.Windows.Forms.Label();
+            this.jarritosNameLabel = new System.Windows.Forms.Label();
+            this.sevenNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cokePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spritePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mountainDewPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jarritosPictueBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenUpPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cokePrice
+            // 
+            this.cokePrice.AutoSize = true;
+            this.cokePrice.Location = new System.Drawing.Point(99, 50);
+            this.cokePrice.Name = "cokePrice";
+            this.cokePrice.Size = new System.Drawing.Size(34, 13);
+            this.cokePrice.TabIndex = 0;
+            this.cokePrice.Text = "$1.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Drinks left:";
+            // 
+            // cokeDrinks
+            // 
+            this.cokeDrinks.Location = new System.Drawing.Point(99, 78);
+            this.cokeDrinks.Name = "cokeDrinks";
+            this.cokeDrinks.Size = new System.Drawing.Size(35, 13);
+            this.cokeDrinks.TabIndex = 2;
+            this.cokeDrinks.Text = "20";
+            // 
+            // cokePictureBox
+            // 
+            this.cokePictureBox.Image = global::drinkVendingMachine.Properties.Resources.coke;
+            this.cokePictureBox.Location = new System.Drawing.Point(15, 21);
+            this.cokePictureBox.Name = "cokePictureBox";
+            this.cokePictureBox.Size = new System.Drawing.Size(78, 70);
+            this.cokePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cokePictureBox.TabIndex = 3;
+            this.cokePictureBox.TabStop = false;
+            this.cokePictureBox.Click += new System.EventHandler(this.cokePictureBox_Click);
+            // 
+            // spritePictureBox
+            // 
+            this.spritePictureBox.Image = global::drinkVendingMachine.Properties.Resources.sprite;
+            this.spritePictureBox.Location = new System.Drawing.Point(192, 21);
+            this.spritePictureBox.Name = "spritePictureBox";
+            this.spritePictureBox.Size = new System.Drawing.Size(78, 70);
+            this.spritePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.spritePictureBox.TabIndex = 4;
+            this.spritePictureBox.TabStop = false;
+            this.spritePictureBox.Click += new System.EventHandler(this.spritePictureBox_Click);
+            // 
+            // spritePrice
+            // 
+            this.spritePrice.AutoSize = true;
+            this.spritePrice.Location = new System.Drawing.Point(277, 50);
+            this.spritePrice.Name = "spritePrice";
+            this.spritePrice.Size = new System.Drawing.Size(34, 13);
+            this.spritePrice.TabIndex = 5;
+            this.spritePrice.Text = "$1.00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Drinks left:";
+            // 
+            // spriteDrinks
+            // 
+            this.spriteDrinks.Location = new System.Drawing.Point(276, 78);
+            this.spriteDrinks.Name = "spriteDrinks";
+            this.spriteDrinks.Size = new System.Drawing.Size(35, 13);
+            this.spriteDrinks.TabIndex = 7;
+            this.spriteDrinks.Text = "20";
+            this.spriteDrinks.UseWaitCursor = true;
+            // 
+            // mountainDewPictureBox
+            // 
+            this.mountainDewPictureBox.Image = global::drinkVendingMachine.Properties.Resources.mountainDew;
+            this.mountainDewPictureBox.Location = new System.Drawing.Point(375, 21);
+            this.mountainDewPictureBox.Name = "mountainDewPictureBox";
+            this.mountainDewPictureBox.Size = new System.Drawing.Size(78, 70);
+            this.mountainDewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mountainDewPictureBox.TabIndex = 8;
+            this.mountainDewPictureBox.TabStop = false;
+            this.mountainDewPictureBox.Click += new System.EventHandler(this.mountainDewPictureBox_Click);
+            // 
+            // dewPrice
+            // 
+            this.dewPrice.AutoSize = true;
+            this.dewPrice.Location = new System.Drawing.Point(460, 50);
+            this.dewPrice.Name = "dewPrice";
+            this.dewPrice.Size = new System.Drawing.Size(34, 13);
+            this.dewPrice.TabIndex = 9;
+            this.dewPrice.Text = "$1.50";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(459, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Drinks left:";
+            // 
+            // dewDrinks
+            // 
+            this.dewDrinks.Location = new System.Drawing.Point(459, 78);
+            this.dewDrinks.Name = "dewDrinks";
+            this.dewDrinks.Size = new System.Drawing.Size(35, 13);
+            this.dewDrinks.TabIndex = 11;
+            this.dewDrinks.Text = "20";
+            // 
+            // jarritosPictueBox
+            // 
+            this.jarritosPictueBox.Image = global::drinkVendingMachine.Properties.Resources.jarritos;
+            this.jarritosPictueBox.Location = new System.Drawing.Point(110, 143);
+            this.jarritosPictueBox.Name = "jarritosPictueBox";
+            this.jarritosPictueBox.Size = new System.Drawing.Size(78, 70);
+            this.jarritosPictueBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.jarritosPictueBox.TabIndex = 12;
+            this.jarritosPictueBox.TabStop = false;
+            this.jarritosPictueBox.Click += new System.EventHandler(this.jarritosPictueBox_Click);
+            // 
+            // sevenUpPictureBox
+            // 
+            this.sevenUpPictureBox.Image = global::drinkVendingMachine.Properties.Resources.sevenUp;
+            this.sevenUpPictureBox.Location = new System.Drawing.Point(297, 143);
+            this.sevenUpPictureBox.Name = "sevenUpPictureBox";
+            this.sevenUpPictureBox.Size = new System.Drawing.Size(78, 70);
+            this.sevenUpPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sevenUpPictureBox.TabIndex = 13;
+            this.sevenUpPictureBox.TabStop = false;
+            // 
+            // jarPrice
+            // 
+            this.jarPrice.AutoSize = true;
+            this.jarPrice.Location = new System.Drawing.Point(194, 172);
+            this.jarPrice.Name = "jarPrice";
+            this.jarPrice.Size = new System.Drawing.Size(34, 13);
+            this.jarPrice.TabIndex = 14;
+            this.jarPrice.Text = "$1.25";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(194, 187);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(57, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Drinks left:";
+            // 
+            // jarritosDrinks
+            // 
+            this.jarritosDrinks.Location = new System.Drawing.Point(194, 200);
+            this.jarritosDrinks.Name = "jarritosDrinks";
+            this.jarritosDrinks.Size = new System.Drawing.Size(41, 13);
+            this.jarritosDrinks.TabIndex = 16;
+            this.jarritosDrinks.Text = "20";
+            // 
+            // sevenPrice
+            // 
+            this.sevenPrice.AutoSize = true;
+            this.sevenPrice.Location = new System.Drawing.Point(381, 172);
+            this.sevenPrice.Name = "sevenPrice";
+            this.sevenPrice.Size = new System.Drawing.Size(34, 13);
+            this.sevenPrice.TabIndex = 17;
+            this.sevenPrice.Text = "$2.00";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(381, 187);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(57, 13);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "Drinks left:";
+            // 
+            // sevenUpDrinks
+            // 
+            this.sevenUpDrinks.Location = new System.Drawing.Point(381, 200);
+            this.sevenUpDrinks.Name = "sevenUpDrinks";
+            this.sevenUpDrinks.Size = new System.Drawing.Size(41, 13);
+            this.sevenUpDrinks.TabIndex = 19;
+            this.sevenUpDrinks.Text = "20";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(186, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Total Sales:";
+            // 
+            // salesLabel
+            // 
+            this.salesLabel.Location = new System.Drawing.Point(266, 258);
+            this.salesLabel.Name = "salesLabel";
+            this.salesLabel.Size = new System.Drawing.Size(100, 13);
+            this.salesLabel.TabIndex = 21;
+            // 
+            // cokeNameLabel
+            // 
+            this.cokeNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cokeNameLabel.Location = new System.Drawing.Point(100, 21);
+            this.cokeNameLabel.Name = "cokeNameLabel";
+            this.cokeNameLabel.Size = new System.Drawing.Size(70, 15);
+            this.cokeNameLabel.TabIndex = 22;
+            this.cokeNameLabel.Text = "Coke";
+            // 
+            // spriteNameLabel
+            // 
+            this.spriteNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spriteNameLabel.Location = new System.Drawing.Point(280, 21);
+            this.spriteNameLabel.Name = "spriteNameLabel";
+            this.spriteNameLabel.Size = new System.Drawing.Size(70, 15);
+            this.spriteNameLabel.TabIndex = 23;
+            this.spriteNameLabel.Text = "Sprite";
+            // 
+            // dewNameLabel
+            // 
+            this.dewNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dewNameLabel.Location = new System.Drawing.Point(463, 21);
+            this.dewNameLabel.Name = "dewNameLabel";
+            this.dewNameLabel.Size = new System.Drawing.Size(94, 15);
+            this.dewNameLabel.TabIndex = 24;
+            this.dewNameLabel.Text = "Mountain Dew";
+            // 
+            // jarritosNameLabel
+            // 
+            this.jarritosNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jarritosNameLabel.Location = new System.Drawing.Point(197, 143);
+            this.jarritosNameLabel.Name = "jarritosNameLabel";
+            this.jarritosNameLabel.Size = new System.Drawing.Size(70, 15);
+            this.jarritosNameLabel.TabIndex = 25;
+            this.jarritosNameLabel.Text = "Jarritos";
+            // 
+            // sevenNameLabel
+            // 
+            this.sevenNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sevenNameLabel.Location = new System.Drawing.Point(384, 143);
+            this.sevenNameLabel.Name = "sevenNameLabel";
+            this.sevenNameLabel.Size = new System.Drawing.Size(70, 15);
+            this.sevenNameLabel.TabIndex = 26;
+            this.sevenNameLabel.Text = "Seven Up";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 289);
+            this.Controls.Add(this.sevenNameLabel);
+            this.Controls.Add(this.jarritosNameLabel);
+            this.Controls.Add(this.dewNameLabel);
+            this.Controls.Add(this.spriteNameLabel);
+            this.Controls.Add(this.cokeNameLabel);
+            this.Controls.Add(this.salesLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sevenUpDrinks);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.sevenPrice);
+            this.Controls.Add(this.jarritosDrinks);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.jarPrice);
+            this.Controls.Add(this.sevenUpPictureBox);
+            this.Controls.Add(this.jarritosPictueBox);
+            this.Controls.Add(this.dewDrinks);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dewPrice);
+            this.Controls.Add(this.mountainDewPictureBox);
+            this.Controls.Add(this.spriteDrinks);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.spritePrice);
+            this.Controls.Add(this.spritePictureBox);
+            this.Controls.Add(this.cokePictureBox);
+            this.Controls.Add(this.cokeDrinks);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cokePrice);
+            this.Name = "Form1";
+            this.Text = "Drinking Vending Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.cokePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spritePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mountainDewPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jarritosPictueBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenUpPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cokePrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label cokeDrinks;
+        private System.Windows.Forms.PictureBox cokePictureBox;
+        private System.Windows.Forms.PictureBox spritePictureBox;
+        private System.Windows.Forms.Label spritePrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label spriteDrinks;
+        private System.Windows.Forms.PictureBox mountainDewPictureBox;
+        private System.Windows.Forms.Label dewPrice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label dewDrinks;
+        private System.Windows.Forms.PictureBox jarritosPictueBox;
+        private System.Windows.Forms.PictureBox sevenUpPictureBox;
+        private System.Windows.Forms.Label jarPrice;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label jarritosDrinks;
+        private System.Windows.Forms.Label sevenPrice;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label sevenUpDrinks;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label salesLabel;
+        private System.Windows.Forms.Label cokeNameLabel;
+        private System.Windows.Forms.Label spriteNameLabel;
+        private System.Windows.Forms.Label dewNameLabel;
+        private System.Windows.Forms.Label jarritosNameLabel;
+        private System.Windows.Forms.Label sevenNameLabel;
+    }
+}
+
---
+++ drinkVendingMachine/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drinkVendingMachine
+{
+    //Create a struct for each of the classes 
+    public struct Soda
+    {
+        public string name;
+        public int amount;
+        public int price;
+    }
+
+    public partial class Form1 : Form
+    {
+        //Create the varaible that will hold the sales
+        double totalSales;
+        const string otherOption = "There are no more perhaps another choice?....";
+        Soda[] sodas = new Soda[]
+        {
+            coke, sprite, mountainDew, sevenUp, jarritos 
+        };
+        private static Soda coke;
+        private static Soda sprite;
+        private static Soda mountainDew;
+        private static Soda sevenUp;
+        private static Soda jarritos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //When the picture is clicked the amount of sodas should go down and the total sales number should go up
+        private void cokePictureBox_Click(object sender, EventArgs e)
+        {
+            //display them within the labels 
+            try
+            {
+                coke.amount = int.Parse(cokeDrinks.Text);
+                coke.name = cokeNameLabel.Text;
+                //Find a way to assign the price of the sodas.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //increment the total sales by the price of the soda and decrement the amount of drinks
+            totalSales += 1;
+            coke.amount -= 1;
+            //display the total sales in the label and the drinks sales
+            salesLabel.Text = "$" + totalSales.ToString();
+            cokeDrinks.Text = coke.amount.ToString();
+
+            //Display a message for when the soda amount is empty
+            if (coke.amount == 0) {
+                MessageBox.Show(otherOption);
+            }
+        }
+
+        private void spritePictureBox_Click(object sender, EventArgs e)
+        {
+            //display
+            try
+            {
+                sprite.amount = int.Parse(spriteDrinks.Text);
+                sprite.name = spriteNameLabel.Text;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            totalSales += 1;
+            sprite.amount -= 1;
+            //Display the total sales in the label and the drinks sales 
+            salesLabel.Text = "$" + totalSales.ToString();
+            spriteDrinks.Text = sprite.amount.ToString();
+
+            //Make sure that the number does not go below 0
+            if (sprite.amount == 0) {
+                MessageBox.Show(otherOption);
+            }
+
+        }
+
+        private void mountainDewPictureBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mountainDew.name = dewNameLabel.Text;
+                mountainDew.amount = int.Parse(dewDrinks.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            totalSales += 1.50;
+            mountainDew.amount -= 1;
+
+            salesLabel.Text = "$" + totalSales.ToString();
+            dewDrinks.Text = mountainDew.amount.ToString();
+
+            if (mountainDew.amount == 0) {
+                MessageBox.Show(otherOption);
+            }
+        }
+
+        private void jarritosPictueBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                jarritos.name = jarritosNameLabel.Text;
+                jarritos.amount = int.Parse(jarritosDrinks.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            totalSales += 1.25;
+            jarritos.amount -= 1;
+
+            salesLabel.Text = "$" + totalSales.ToString();
+            jarritosDrinks.Text = jarritos.amount.ToString();
+
+            if (jarritos.amount == 0) {
+                MessageBox.Show(otherOption);
+            }
+        }
+    }
+}
